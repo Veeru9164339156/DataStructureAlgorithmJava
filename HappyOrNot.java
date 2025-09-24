7edcef5c90734590e3d06433453cbe247e313671
Veeresh K C <155379894+Veeru9164339156@users.noreply.github.com>
2025-09-24 15:09:46 +0530

    recursion_progrms

---
+++ HappyOrNot.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class HappyOrNot {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	System.out.println("eter the number");
+	int n=sc.nextInt();
+	if(isHappyNo(n))
+		System.out.println("haPPY no");
+	else
+		System.out.println("not a happy no");	
+	}
+static boolean isHappyNo(int n) {
+	if(n==1)
+	
+}
+}

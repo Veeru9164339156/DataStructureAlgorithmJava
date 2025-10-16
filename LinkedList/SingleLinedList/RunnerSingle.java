6b61de41aa6353ccbe5c33e78cf799f7c1819363
Veeresh K C <155379894+veeresh-kc@users.noreply.github.com>
2025-10-16 20:01:39 +0530

    Refactor RunnerSingle and implement SingleLinkedListImpl
    Refactored RunnerSingle class and added SingleLinkedListImpl class with additional linked list operations.

--- LinkedList/SingleLinedList/RunnerSingle.java
+++ LinkedList/SingleLinedList/RunnerSingle.java
@@ -1,12 +1,31 @@
-package dsa;
 
-public class RunnerSingle {
-	public static void main(String[] args) {
-		SingleLinkedList sl=new SingleLinkedList();
-		sl.add(25);
-		sl.add(38);
-		sl.add(45);
-		sl.add(18);
-		sl.display();
-	}
+public class SingleLinkedListImpl {
+public static void main(String[] args) {
+	SingleLinkedList sl=new SingleLinkedList();
+	sl.add(10);
+	sl.add(20);
+	sl.add(30);
+	sl.add(50);
+//	sl.display();
+	sl.addFirst(5);
+	sl.addFirst(2);
+	sl.addFirst(1);
+//	sl.display();
+//	System.out.println(sl.deleteFirst());
+//	System.out.println(sl.deleteFirst());
+//	System.out.println(sl.deleteLast());
+//	sl.display();
+//	sl.add(142,2);
+//	sl.add(200,0);
+//	System.out.println(sl.delete(2));
+//	sl.display();
+//	sl.reverse();
+	sl.display();
+//	System.out.println(sl.size());
+	System.out.println(sl.getMiddleNode());
+	System.out.println(sl.hasLoop());
+	
+	
+}
+
 }
